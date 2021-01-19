5dfca1b9c0a521020ff8e0b9bbf332f683be62a7
unknown <varunvardhan86@gmail.com>
2021-01-19 19:31:19 +0530

    First intial project setup

---
+++ LearnWebAppWS/LearnWebAppWS/src/test/java/com/LearnWebAppWS/MobileAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnWebAppWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
